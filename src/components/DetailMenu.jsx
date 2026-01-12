634fabd69a7567bd096b7e634c217a7e1fc16d5f
unknown <wrpxdouble1135@gmail.com>
2026-01-12 22:12:15 +0700

    modified page

--- src/components/DetailMenu.jsx
+++ src/components/DetailMenu.jsx
@@ -2,16 +2,29 @@ import { Link } from "react-router";
 
 export default function DetailMenu() {
   return (
-    <div className="relative">
+    <section className="flex w-auto">
+      <div className="relative flex px-30 py-5">
         <img
-          className="object-cover w-full h-200"
+          className="object-cover h-100 "
           src="https://media.istockphoto.com/id/1301094972/photo/hands-of-a-woman-holding-a-mobile-phone-above-a-cutting-board-full-of-vegetables.jpg?s=612x612&w=0&k=20&c=uLnPaPrhl3DnicrKZwvljxfxfv9sBjAmHxoqzS_-zmE="
-          width={500} height={500}
         />
-        <div className="absolute inset-90 flex-col text-center ">
-        <p className="text-5xl font-bold mb-10 ">Discover What to Cook Today</p>
-        <button class="btn" href="#" className="px-6 py-4 text-2xl text-yellow-50 bg-black font-medium rounded-2xl hover:bg-gray-900 ">View The Menu</button>
+        <img
+          className="object-cover h-100"
+          src="https://media.istockphoto.com/id/809933218/photo/making-christmas-dinner.jpg?s=612x612&w=0&k=20&c=4UvJkQeX8ClZ-jf_o99HIz0JNNhyHHjg3k_AdzaqKpM="
+        />
+        <div className="absolute inset-33 flex-col text-center ">
+          <p className="text-5xl font-bold mb-10 ">
+            Discover What to Cook Today
+          </p>
+          <button
+            class="btn"
+            href="#"
+            className="px-6 py-4 text-2xl text-yellow-50 bg-black font-medium rounded-2xl hover:bg-gray-900 "
+          >
+            View The Menu
+          </button>
         </div>
-    </div>
+      </div>
+    </section>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router";
 export default function Hero() {
   return (
     <div className="">
-      <div className="flex gap-10 p-15">
+      <div className="flex gap-10 pl-30 pr-20 py-10">
         <img
           className="w-200 rounded-3xl shadow"
           src="https://www.shutterstock.com/image-photo/create-static-showing-chicken-curry-600nw-2648052515.jpg"
         />
-        <section className="self-center">
+        <section className="self-center pl-10">
           <p className="text-6xl text-orange-400 font-extrabold ">MenuCraft</p>
           <br />
           <h1 className="text-5xl text-yellow-100 font-bold">Simple Recipes. Better Meals</h1>
